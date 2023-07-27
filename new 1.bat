6ed66305587782a304dc77754919fdeb4123bdde
Nguyễn Thăng Long <114731821+NTLong-Dev@users.noreply.github.com>
2023-07-28 00:35:13 +0700

    Add files via upload

---
+++ new 1.bat
@@ -0,0 +1,151 @@
+@echo off
+mode 800
+color 0A
+:start
+
+echo ____________36936936936936936
+PING localhost -n 2 -w 1.0 >Nul
+echo ____________36936936936936936
+PING localhost -n 2 -w 1.0 >Nul
+echo ____________369369369369369369
+PING localhost -n 2 -w 1.0 >Nul
+echo ___________36936936936936933693
+PING localhost -n 2 -w 1.0 >Nul
+echo __________3693693693693693693693
+PING localhost -n 2 -w 1.0 >Nul
+echo _________369369369369369369369369
+PING localhost -n 2 -w 1.0 >Nul
+echo _________3693693___LINH____3693699
+PING localhost -n 2 -w 1.0 >Nul
+echo ________3693693693693693693693699369
+PING localhost -n 2 -w 1.0 >Nul
+echo _______36936939693693693693693693693693
+PING localhost -n 2 -w 1.0 >Nul
+echo _____3693693693693693693693693693693636936
+PING localhost -n 2 -w 1.0 >Nul
+echo ___36936936936936936936936936936___369369369
+PING localhost -n 2 -w 1.0 >Nul
+echo __36936___369336936369369369369________36936
+PING localhost -n 2 -w 1.0 >Nul
+echo _36936___36936_369369336936936__¶¶__¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo 36933___36936__36936___3693636_¶¶¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo 693____36936__36936_____369363_¶¶¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo ______36936__36936______369369__¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo _____36936___36936_______36936___¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo _____36936___36936________36936___¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo _____36936___36936_________36936___11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ______369____36936__________369___11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ______________369________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo _______________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ___________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ________________________¶¶¶_¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo _______________________¶¶¶¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo _______________________¶¶¶¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo ________________________¶¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo _________________________¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo __________________________¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo ___________________________¶
+PING localhost -n 2 -w 1.0 >Nul
+echo ______________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ________________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo __________________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ___________________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ___________________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo __________________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo _________________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo _______________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ___________________________¶¶__¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo __________________________¶¶¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo __________________________¶¶¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo ___________________________¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo ____________________________ ¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo _____________________________ ¶
+PING localhost -n 2 -w 1.0 >Nul
+echo ____________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo __________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo _________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ___________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo _____________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ________________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo __________________________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ______________369___________________11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ______369____36936__________369_____11,
+PING localhost -n 2 -w 1.0 >Nul
+echo _____36936___36936_________36936___11,
+PING localhost -n 2 -w 1.0 >Nul
+echo _____36936___36936________36936___11,
+PING localhost -n 2 -w 1.0 >Nul
+echo _____36936___36936_______36936___11,
+PING localhost -n 2 -w 1.0 >Nul
+echo ______36936__36936______369369 _¶¶_¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo 693____36936__36936_____369363 ¶¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo 36933___36936__36936___3693636 ¶¶¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo _36936___36936_369369336936936 _¶¶¶¶¶
+PING localhost -n 2 -w 1.0 >Nul
+echo __36936___369336936369369369369 _¶¶¶__3696
+PING localhost -n 2 -w 1.0 >Nul
+echo ___36936936936936936936936936936 _¶_336939
+PING localhost -n 2 -w 1.0 >Nul
+echo _____36936936936936936936936936936936936
+PING localhost -n 2 -w 1.0 >Nul
+echo _______369369396936936936936936693693
+PING localhost -n 2 -w 1.0 >Nul
+echo ________36936936936936936936999369
+PING localhost -n 2 -w 1.0 >Nul
+echo _________3693693___Rong___6933699
+PING localhost -n 2 -w 1.0 >Nul
+echo _________3693693693693693369369
+PING localhost -n 2 -w 1.0 >Nul
+echo __________36936936936936993693
+PING localhost -n 2 -w 1.0 >Nul
+echo ___________369369369369333693
+PING localhost -n 2 -w 1.0 >Nul
+echo ____________3693693693699369
+PING localhost -n 2 -w 1.0 >Nul
+echo ____________369369369366936
+PING localhost -n 2 -w 1.0 >Nul
+echo ____________36936936936693
+PING localhost -n 2 -w 1.0 >Nul
+
+pause
